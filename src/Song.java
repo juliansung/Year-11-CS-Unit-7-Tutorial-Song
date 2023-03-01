5f2afd98f4f601c140ebd1f1d2c77b81adec52d1
juliansung <julians2025@student.cis.edu.hk>
2023-03-01 08:27:59 +0800

    push

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String artist;
+    private String title;
+
+    private ArrayList<String> newListeners;
+
+    public Song(String title, String artist){
+        this.artist=artist;
+        this.title=title;
+        newListeners = new ArrayList();
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public int howMany(ArrayList<String> list) {
+        int count = 0;
+        for (String string : list) {
+            String name = string.toLowerCase();
+            if (!newListeners.contains(name)) {
+                newListeners.add(name);
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
